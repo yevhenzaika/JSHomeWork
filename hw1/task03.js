cd23a4737fd6a926649c42203452130508e9634a
Yevhen Zaika <evgenij.zajka@privatbank.ua>
2022-01-18 20:32:18 +0200

    add

---
+++ hw1/task03.js
@@ -0,0 +1,9 @@
+var val = 'e';
+
+if (val == 'a') {
+    console.log('a');
+} else if (val == 'b' || val == 'c' || val == 'd' || val == 'e') {
+    console.log("others");
+} else {
+    console.log("unknown");
+}
