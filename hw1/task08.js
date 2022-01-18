3670b070658ada6528c2ab8540733f47a00b4aa1
Yevhen Zaika <evgenij.zajka@privatbank.ua>
2022-01-18 20:50:47 +0200

    add

--- hw1/task08.js
+++ hw1/task08.js
@@ -1,8 +1,9 @@
 var arr = [1, 2, 3, 4];
+
 var sum = 0;
 for (var i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 == 0) {
+    if ((arr[i] % 2 == 0) && (arr[i] > 3))
         sum += arr[i];
-    }
 }
+
 console.log(sum);
